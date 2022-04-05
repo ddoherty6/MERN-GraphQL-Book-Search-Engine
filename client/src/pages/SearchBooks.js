0008b4ad5361d81b23c3ea7417575dacd041e77a
klem <ddoherty6@gmail.com>
2022-04-05 15:13:44 -0700

    committing to transfer to laptop

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,8 +4,9 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 // import { GET_ME } from '../utils/queries';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -60,14 +61,17 @@ const SearchBooks = () => {
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null; ///////////////////////////////////////////////////////////////////////////
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
       return false;
     }
 
     try {
-      const response = await saveBook(bookToSave, token);  ///////////////////////////////////////////////////////////////
+      // const response = await saveBook(bookToSave, token);  ///////////////////////////////////////////////////////////////
+      const [saveBookMutation, { data, loading, error }] =  useMutation(SAVE_BOOK);
+
+      const response = saveBookMutation();
 
       if (!response.ok) {
         throw new Error('something went wrong!');
